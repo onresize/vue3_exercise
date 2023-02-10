5b4905b65557d95d226a918ae711bf51900583f1
Embrance-T <2069814988@qq.com>
2023-02-10 16:14:57 +0800

    fix:更新

--- src/components/唯一id/index.vue
+++ src/components/唯一id/index.vue
@@ -8,16 +8,28 @@
 
 <script setup>
 // defineProps defineEmits defineExpose withDefaults onMounted onUnmounted
-import {
-  ref,
-  reactive,
-  computed,
-  watch,
-  watchEffect,
-  defineAsyncComponent,
-} from "vue";
+import { reactive, onMounted } from "vue";
 
-console.log("唯一id:", crypto.randomUUID());
+// console.log("唯一id:", crypto.randomUUID());
+let obj = reactive({ arr: [1, 2, 3, 4, 5, 6] });
+
+// 写多个if判断时、if内部有定时器、return放定时器内外的区别
+onMounted(() => {
+  for (let i = 0; i < obj.arr.length; i++) {
+    if (obj.arr[i] == 1) {
+      setTimeout(() => {
+        console.log(111111);
+      }, 1000);
+      return;
+    }
+    if (obj.arr[i] == 2) {
+      console.log(222222);
+    }
+    if (obj.arr[i] == 3) {
+      console.log(333333);
+    }
+  }
+});
 </script>
 
 <style scoped></style>
