ae0c7b7a67ab146c2fd4690733d6ffd5821033ef
Embrace <2069814988@qq.com>
2023-05-09 13:05:09 +0800

    fix:xx

--- src/components/全局变量和函数/index.vue
+++ src/components/全局变量和函数/index.vue
@@ -8,7 +8,8 @@
 <script setup lang="ts">
 import { getCurrentInstance, ComponentInternalInstance } from "vue";
 const { appContext, proxy } = <ComponentInternalInstance>getCurrentInstance();
-console.log("setup中拿到全局变量 -->", appContext.config.globalProperties.$env);
+console.log("JS中拿到全局变量$env -->", appContext.config.globalProperties.$env);
+console.log("JS中拿到全局变量$filters -->", appContext.config.globalProperties.$filters.format("vue3移除过滤器filters属性"));
 // console.log("环境变量", import.meta.env.VITE_TITLE);
 console.log("代理：", proxy);
 </script>
